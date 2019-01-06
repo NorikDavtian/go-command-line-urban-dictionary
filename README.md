39088e15990a708016423ded81dc6b9b3891e78a
Norik Davtian <norik@norik.io>
2019-01-05 23:15:51 -0800

    call to urban dictionary

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Go Command Line Urban Dictionary
 Just a basic go command line example that find a definition of a word from Urban Dictionary API and displays the first result to the user.
 
+`	url := fmt.Sprintf("http://api.urbandictionary.com/v0/define?term=%s", *wordPtr)`
+
 ## Start
 ```
 ➜  go-command-line-urban-dictionary git:(master) ✗ go build .
@@ -12,7 +14,20 @@ Usage of ./go-command-line-urban-dictionary:
 use the `-word` flag to pass in the word
 ```
 ➜  go-command-line-urban-dictionary git:(master) ✗ ./go-command-line-urban-dictionary -word=yolo
-word: yolo
-➜  go-command-line-urban-dictionary git:(master) ✗
+Word: YOLO
+Defenition: An overused acronym for "[You only live once]." There is an [exception] for those who believe in [reincarnation] or are cats.
+Example: Examples:
+—I got up.
+#[yolo]
+—I inhaled.
+#yolo
+—I exhaled.
+#yolo
+—I [took a shit].
+#yolo
+—I [flushed] the toilet.
+#yolo
+—I stretched.
+#yolo
 ```
 
--- main.go
+++ main.go
@@ -4,8 +4,24 @@ package main
 // command-line flag parsing. We'll use this package to
 // implement our example command-line program.
 // Read more: https://gobyexample.com/command-line-flags
-import "flag"
-import "fmt"
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type defenitionResponse struct {
+	Definition string `json:"definition"`
+	Word       string `json:"word"`
+	Example    string `json:"example"`
+}
+
+type listResponse struct {
+	List []defenitionResponse `json:"list"`
+}
 
 func main() {
 
@@ -25,5 +41,37 @@ func main() {
 	// any trailing positional arguments. Note that we
 	// need to dereference the pointers with e.g. `*wordPtr`
 	// to get the actual option values.
-	fmt.Println("word:", *wordPtr)
+	url := fmt.Sprintf("http://api.urbandictionary.com/v0/define?term=%s", *wordPtr)
+
+	// Check for HTTP error on connection
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println(errors.New("Failed to connect to Urban Dictionary API"))
+	}
+
+	// Check for HTTP status code
+	if res.StatusCode != 200 {
+		fmt.Println(fmt.Errorf("Failed with HTTP Code: %d", res.StatusCode))
+	}
+
+	// Read response body
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		fmt.Println(fmt.Errorf("Failed to read body from response"))
+	}
+
+	// Parse response
+	var response listResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		fmt.Println(fmt.Errorf("Failed to unmarshal JSON: %s", body))
+	}
+
+	firstDefenition := response.List[0]
+
+	fmt.Printf("Word: %s\n", firstDefenition.Word)
+	fmt.Printf("Defenition: %s\n", firstDefenition.Definition)
+	fmt.Printf("Example: %s\n", firstDefenition.Example)
+
 }
